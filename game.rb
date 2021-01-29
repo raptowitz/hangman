b58dac7f9e390fe2e5caf70761be54b3757c0ecd
Rachael Aptowitz <raptowitz@gmail.com>
2021-01-28 20:36:55 -0800

    case insensitive input

--- game.rb
+++ game.rb
@@ -2,22 +2,21 @@
 
 # game logic
 class Game
-  def initialize(player, board)
-    @player = player
-    @board = board
-    @secret_word = nil
+  def initialize
+    @player = Player.new
+    @board = Board.new(create_secret_word)
   end
 
   def play_game
-    create_secret_word
-    @board.display(@secret_word)
-    find_index if check_word(@player.guess)
+    @board.display
+    find_letters if check_word(@player.guess)
+    @board.display
   end
 
   def create_secret_word
     words = File.readlines('5desk.txt')
     words.keep_if { |word| word.chomp.length > 5 && word.chomp.length < 12 }
-    @secret_word = words.sample.chomp
+    @secret_word = words.sample.chomp.downcase
     p @secret_word
   end
 
@@ -26,7 +25,7 @@ class Game
     @secret_word.include?(@guess)
   end
 
-  def find_index
+  def find_letters
     @secret_word.split(//).each_with_index do |letter, index|
       @board.place_guess(letter, index) if letter == @guess
     end
@@ -36,15 +35,19 @@ end
 # human player
 class Player
   def guess
-    gets.chomp
+    gets.chomp.downcase
   end
 end
 
 # display
 class Board
-  def display(secret_word)
+  def initialize(secret_word)
     @secret_word = secret_word
-    p @display = Array.new(secret_word.length, '?')
+    @display = Array.new(secret_word.length, '_')
+  end
+
+  def display
+    puts @display.join(' ')
   end
 
   def place_guess(letter, index)
@@ -52,8 +55,7 @@ class Board
     @index = index
     @display[@index] = @letter
   end
-
 end
 
-new_game = Game.new(Player.new, Board.new)
+new_game = Game.new
 new_game.play_game
