8420124cd2c7d0d0054068f0500c9b608b75cbf0
Rachael Aptowitz <raptowitz@gmail.com>
2021-01-28 19:03:42 -0800

    check secret word for player guess

--- game.rb
+++ game.rb
@@ -5,18 +5,33 @@ class Game
   def initialize(player, board)
     @player = player
     @board = board
+    @secret_word = nil
   end
 
   def play_game
+    create_secret_word
+    @board.display(@secret_word)
+    check_word(@player.guess)
+  end
+
+  def create_secret_word
     words = File.readlines('5desk.txt')
     words.keep_if { |word| word.chomp.length > 5 && word.chomp.length < 12 }
-    secret_word = words.sample.chomp
-    @board.display(secret_word)
+    @secret_word = words.sample.chomp
+    p @secret_word
   end
+
+  def check_word(guess)
+    @secret_word.include?(guess)
+  end
+
 end
 
 # human player
 class Player
+  def guess
+    gets.chomp
+  end
 end
 
 # display
