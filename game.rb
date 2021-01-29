16e08951e106c066aff2a426d0c274a36ffdda2f
Rachael Aptowitz <raptowitz@gmail.com>
2021-01-28 18:32:56 -0800

    display method for board

--- game.rb
+++ game.rb
@@ -2,12 +2,30 @@
 
 # game logic
 class Game
+  def initialize(player, board)
+    @player = player
+    @board = board
+  end
+
   def play_game
     words = File.readlines('5desk.txt')
     words.keep_if { |word| word.chomp.length > 5 && word.chomp.length < 12 }
-    puts words.sample(1)
+    secret_word = words.sample.chomp
+    @board.display(secret_word)
   end
 end
 
-new_game = Game.new
+# human player
+class Player
+end
+
+# display
+class Board
+  def display(secret_word)
+    @secret_word = secret_word
+    p Array.new(secret_word.length, '?')
+  end
+end
+
+new_game = Game.new(Player.new, Board.new)
 new_game.play_game
