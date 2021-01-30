292053e62a0ea34769468c7264b102ff81ae4f8d
Rachael Aptowitz <raptowitz@gmail.com>
2021-01-29 20:38:34 -0800

    validate player guesses

--- game.rb
+++ game.rb
@@ -6,7 +6,7 @@ module Dictionary
     words = File.readlines('5desk.txt')
     words.keep_if { |word| word.chomp.length > 5 && word.chomp.length < 12 }
     @secret_word = words.sample.chomp.downcase
-    p @secret_word
+    # p @secret_word
   end
 end
 
@@ -17,20 +17,25 @@ class Game
   def initialize(board, player)
     @player = player
     @board = board
-    @wrong_letters = []
-    @lives = 8
+  end
+
+  def start_game
+    @board.blank_board(secret_word)
+    until @board.lives.zero?
+      play_game
+      break if @board.game_over?
+    end
+    display_results
   end
 
   def play_game
-    @board.blank_board(secret_word)
-    until @lives.zero?
-      if word_contains(@player.guess)
-        find_letter
-      else
-        wrong_letter
-      end
-      @board.display
+    if word_contains(@player.guess)
+      find_letter
+    else
+      @board.wrong_letter(@guess)
     end
+    puts "\e[H\e[2J"
+    @board.display
   end
 
   def word_contains(guess)
@@ -44,22 +49,47 @@ class Game
     end
   end
 
-  def wrong_letter
-    @wrong_letters.push(@guess)
-    puts "\nUsed Letters: #{@wrong_letters.join(' ')}"
-    puts "#{@lives -= 1} guesses left"
+  def display_results
+    if @board.game_over?
+      puts "\nYou win! \nThe secret word was #{@secret_word}"
+    else
+      puts "\nTry again! \nThe secret word was #{@secret_word}"
+    end
   end
 end
 
 # human player
 class Player
+  def initialize
+    @guessed_letters = []
+  end
+
   def guess
-    gets.chomp.downcase
+    puts 'Guess a letter of the secret word!'
+    @guess = gets.chomp.downcase
+    validate
+  end
+
+  def validate
+    until @guess.length == 1 && @guess.match?(/[[:alpha:]]/) &&
+          @guessed_letters.none?(@guess)
+      puts 'Guess one letter'
+      @guess = gets.chomp.downcase
+    end
+    @guessed_letters.push(@guess)
+    @guess
   end
 end
 
 # display
 class Board
+  attr_reader :lives, :wrong_letters
+
+  def initialize
+    @wrong_letters = []
+    @lives = 8
+  end
+
   def blank_board(secret_word)
     @secret_word = secret_word
     @display = Array.new(secret_word.length, '_')
@@ -67,7 +97,9 @@ class Board
   end
 
   def display
-    puts @display.join(' ')
+    puts "Used Letters: #{@wrong_letters.join(' ')}"
+    puts "#{@lives} guesses left"
+    puts "\n #{@display.join(' ')}"
   end
 
   def place_guess(letter, index)
@@ -75,7 +107,17 @@ class Board
     @index = index
     @display[@index] = @letter
   end
+
+  def wrong_letter(guess)
+    @guess = guess
+    @wrong_letters.push(@guess)
+    @lives -= 1
+  end
+
+  def game_over?
+    @display.none? { |space| space == '_' }
+  end
 end
 
 new_game = Game.new(Board.new, Player.new)
-new_game.play_game
+new_game.start_game
